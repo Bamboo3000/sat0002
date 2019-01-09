03c84789d965009934b5234bef8530164d5ad2ad
Jakub Pabis <jakubpabis@Jakubs-MacBook-Pro.local>
2019-01-10 00:36:40 +0100

    add some graphics

--- prototype/src/js/app.js
+++ prototype/src/js/app.js
@@ -285,7 +285,7 @@ function lazyImages()
 function wpoints()
 {
     var waypoints = $('section').waypoint(function(direction) {
-        $('header.navigation').find('a[data-href="#'+this.element.id+'"]').addClass('active');
+        $('header.navigation').find('a[data-href="#'+this.element.id+'"]').toggleClass('active');
      }, {
         offset: 60
     });
